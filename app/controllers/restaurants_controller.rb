e8fda4dfc09dc8d9c429b1d810ce902aad458fc0
Daniel Rodriguez <daniel.rod93@gmail.com>
2019-11-27 17:39:14 +0100

    rails yelp mvc without CSS styling

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,27 @@
 class RestaurantsController < ApplicationController
+
   def index
-  end
-
-  def create
-  end
-
-  def new
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+bristol = Restaurant.create(name: "Epicure", category: "chinese", address: "new street")
+barcelona = Restaurant.create(name: "solsoler", category: "chinese", address: "new street")
+hanoi = Restaurant.create(name: "hanoi", category: "chinese", address: "new street")
+paris = Restaurant.create(name: "Epicure", category: "french", address: "new street")
+madrid = Restaurant.create(name: "Epicure", category: "french", address: "new street")
+
+p "#{Restaurant.count} number of restaurants added"
